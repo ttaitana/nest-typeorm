ff6f909065ee3839866762753275bb63498a4d73
ttaitana <taitana.ym@outlook.com>
2021-04-30 14:50:47 +0700

    add booking controller test

--- src/booking/booking.controller.spec.ts
+++ src/booking/booking.controller.spec.ts
@@ -5,16 +5,59 @@ import { BookingService } from './booking.service';
 describe('BookingController', () => {
   let controller: BookingController;
 
+  const mockupBookingService = {
+    create: jest.fn((dto) => {
+      return {
+        id: Date.now(),
+        ...dto,
+      };
+    }),
+    update: jest.fn().mockImplementation((id, dto) => ({
+      id,
+      ...dto,
+    })),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BookingController],
       providers: [BookingService],
-    }).compile();
+    })
+      .overrideProvider(BookingService)
+      .useValue(mockupBookingService)
+      .compile();
 
     controller = module.get<BookingController>(BookingController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create booking', () => {
+    const dto = {
+      hotelName: 'HODHotel',
+      roomNumber: 'P-012',
+      name: 'Tanachai',
+      bookingDate: new Date(Date.now()),
+    };
+    expect(controller.create(dto)).toEqual({
+      id: expect.any(Number),
+      ...dto,
+    });
+  });
+
+  it('should update booking', () => {
+    const dto = {
+      hotelName: 'HODHotel-Patiphat',
+      roomNumber: 'P-011',
+      name: 'Tanachai Saetung',
+      bookingDate: new Date(Date.now()),
+    };
+    expect(controller.update('1', dto)).toEqual({
+      id: 1,
+      ...dto,
+    });
+    expect(mockupBookingService.update).toHaveBeenCalled();
   });
 });
--- src/booking/dto/update-booking.dto.ts
+++ src/booking/dto/update-booking.dto.ts
@@ -1,10 +1,6 @@
 import { IsDate, IsNumber, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+// import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookingDto } from './create-booking.dto';
 
-export class UpdateBookingDto extends PartialType(CreateBookingDto) {
-  @ApiProperty()
-  @IsNumber()
-  id: number;
-}
+export class UpdateBookingDto extends PartialType(CreateBookingDto) {}
